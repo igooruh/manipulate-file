96151ba44f850bdf6cb240b65d5a246116ba78a9
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2022-03-31 15:22:25 -0300

    test id on file json

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const { deepEqual, ok } = require('assert')
 
+const database = require('./database')
+
 const DEFAULT_ITEM_REGISTER = {
     name: 'Flash',
     power: 'Speed',
@@ -11,6 +13,9 @@ describe('Suite of manipilation of heros', () => {
     it('should search a hero, using file', async () => {
         const expected = DEFAULT_ITEM_REGISTER
 
-        ok(null, expected)
+        // Destruct return first position
+        const [result] = await database.list(expected.id)
+
+        ok(result, expected)
     })
 })
